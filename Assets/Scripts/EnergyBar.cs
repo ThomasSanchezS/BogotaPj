aebd6dc8a9c7586b84c7fa7d8f79be56e666da5b
ThomasSanchezS <94389209+ThomasSanchezS@users.noreply.github.com>
2023-04-11 17:08:36 -0500

    Update EnergyBar.cs

--- Assets/Scripts/EnergyBar.cs
+++ Assets/Scripts/EnergyBar.cs
@@ -13,7 +13,6 @@ public class EnergyBar : MonoBehaviour
     public float visibilityMultiplier = 1.0f;
     public float startingSpeed;
     public float startingRunningSpeed;
-    public float startingCrouchingSpeed;
     public Camera cam;
 
     public Image energyBarUI;
@@ -27,7 +26,6 @@ public class EnergyBar : MonoBehaviour
         playerMovement = GetComponent<PlayerMovement>();
         startingSpeed = playerMovement.speed;
         startingRunningSpeed = playerMovement.runSpeed;
-        startingCrouchingSpeed = playerMovement.crouchingSpeed;
          m_Vignette = ScriptableObject.CreateInstance<Vignette>();
          m_Vignette.enabled.Override(true);
          m_Vignette.intensity.Override(0.1f);
@@ -72,16 +70,12 @@ public class EnergyBar : MonoBehaviour
         {
             playerMovement.speed = startingSpeed *((2 * energy) / maxEnergy);
             playerMovement.runSpeed = startingRunningSpeed*((2 * energy) / maxEnergy);
-            playerMovement.crounchingSpeed = startingCrouchingSpeed * ((2* energy) / maxEnergy);
             m_Vignette.intensity.value = 0.4f - (0.3f * (Mathf.Pow((2*energy/maxEnergy),2)));
             if(playerMovement.speed <= 2f){
                 playerMovement.speed = 2f;
             }
             if(playerMovement.runSpeed <= 2.5f){
                  playerMovement.runSpeed = 2.5f;
-            }
-            if(playerMovement.crouchingSpeed <= 2.5f){
-                playerMovement.crouchingSpeed <= 2.5f;
             }
             // Reduce visibility using e.g. post-processing effects
         }
